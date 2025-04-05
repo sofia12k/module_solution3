6d50c4fafad20b40f9500129b2d64757537a0038
sofia12k <145674708+sofia12k@users.noreply.github.com>
2025-04-05 15:37:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -59,44 +59,37 @@
   function MenuSearchService($http) {
     var service = this;
 
-    // Function to get matched menu items from the server
-    service.getMatchedMenuItems = function (searchTerm) {
-      return $http({
-        method: 'GET',
-        url: 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json'  // Firebase URL
-      }).then(function (response) {
-        // Log the entire response to inspect the structure of the returned data
-        console.log('Firebase response:', response.data);
-
-        var allItems = response.data;  // Firebase data (could be an object or array)
-        var foundItems = [];
-
-        // Check if the data returned is an object (common in Firebase)
-        if (typeof allItems === 'object') {
-          // Loop through all the items if the data is an object (Firebase's default response)
-          for (var key in allItems) {
-            if (allItems.hasOwnProperty(key)) {
-              var item = allItems[key];
-              if (item.description && item.description.toLowerCase().includes(searchTerm.toLowerCase())) {
-                foundItems.push(item);  // Add item to foundItems
-              }
-            }
-          }
-        } else if (Array.isArray(allItems)) {
-          // If it's an array, use the previous logic
-          for (var i = 0; i < allItems.length; i++) {
-            if (allItems[i].description && allItems[i].description.toLowerCase().includes(searchTerm.toLowerCase())) {
-              foundItems.push(allItems[i]);  // Add item to foundItems
+  service.getMatchedMenuItems = function (searchTerm) {
+    console.log("Fetching data with search term:", searchTerm);
+    return $http({
+      method: 'GET',
+      url: 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json'
+    }).then(function (response) {
+      console.log('Firebase response:', response.data);
+      var allItems = response.data;  // Firebase data (could be an object or array)
+      var foundItems = [];
+  
+      if (typeof allItems === 'object') {
+        for (var key in allItems) {
+          if (allItems.hasOwnProperty(key)) {
+            var item = allItems[key];
+            if (item.description && item.description.toLowerCase().includes(searchTerm.toLowerCase())) {
+              foundItems.push(item);
             }
           }
         }
-
-        // Return the list of found items
-        return foundItems;  
-      }).catch(function (error) {
-        console.error('Error fetching menu items:', error);
-        return [];  // Return an empty array on error
-      });
-    };
+      } else if (Array.isArray(allItems)) {
+        for (var i = 0; i < allItems.length; i++) {
+          if (allItems[i].description && allItems[i].description.toLowerCase().includes(searchTerm.toLowerCase())) {
+            foundItems.push(allItems[i]);
+          }
+        }
+      }
+      return foundItems;
+    }).catch(function (error) {
+      console.error('Error fetching menu items:', error);
+      return [];
+    });
+  };
   }
 })();
